ab02015a206ea34ef6509fbc69e101e922a846a4
LucasAzais <lucasazais@gmail.com>
2014-05-27 18:29:42 +0200

    Completed the make_order

--- src/mc/classic/make_order.cc
+++ src/mc/classic/make_order.cc
@@ -31,58 +31,62 @@ make_order(const conf::configuration& conf, statistics& stats, const pn::net& ne
   std::cout << std::endl;
 
   std::ifstream file;
-  file.open(conf.matrix_conf_file_path);
+  if(conf.matrix_conf_file_path){
 
-  if(file.good())
-  {
+    file.open(*conf.matrix_conf_file_path);
 
-    int buffer_size = 256;
-    char init_pop[buffer_size];
-    char it_num[buffer_size];
-    char mut_size[buffer_size];
-    char select_size[buffer_size];
+    if(file.good())
+    {
 
-    file.getline(init_pop, buffer_size);
-    file.getline(it_num, buffer_size);
-    file.getline(mut_size, buffer_size);
-    file.getline(select_size, buffer_size);
+      int buffer_size = 256;
+      char init_pop[buffer_size];
+      char it_num[buffer_size];
+      char mut_size[buffer_size];
+      char select_size[buffer_size];
 
-    std::string init_pop_str (init_pop);
-    std::string it_num_str (it_num);
-    std::string mut_size_str (mut_size);
-    std::string select_size_str (select_size);
+      file.getline(init_pop, buffer_size);
+      file.getline(it_num, buffer_size);
+      file.getline(mut_size, buffer_size);
+      file.getline(select_size, buffer_size);
 
-    init_pop_str.erase(0, 21);
-    it_num_str.erase(0, 19);
-    mut_size_str.erase(0, 16);
-    select_size_str.erase(0, 17);;
+      std::string init_pop_str (init_pop);
+      std::string it_num_str (it_num);
+      std::string mut_size_str (mut_size);
+      std::string select_size_str (select_size);
 
-    const unsigned int INITIAL_POPULATION = std::stoi( init_pop_str );
-    const unsigned int ITERATION_NUMBER = std::stoi( it_num_str );
-    const unsigned int MUTATION_SIZE = std::stoi( mut_size_str );
-    const unsigned int SELECTION_SIZE = std::stoi( select_size_str );
-  
-    Population pop;
+      init_pop_str.erase(0, 21);
+      it_num_str.erase(0, 19);
+      mut_size_str.erase(0, 16);
+      select_size_str.erase(0, 17);;
 
-    if(conf.order_ordering_matrix) {
-
-      pop.generate(&matrix, INITIAL_POPULATION);
-
-      for(unsigned int i = 0; i < ITERATION_NUMBER; ++i)
-      {
+      const unsigned int INITIAL_POPULATION = std::stoi( init_pop_str );
+      const unsigned int ITERATION_NUMBER = std::stoi( it_num_str );
+      const unsigned int MUTATION_SIZE = std::stoi( mut_size_str );
+      const unsigned int SELECTION_SIZE = std::stoi( select_size_str );
     
-      pop.mutate(pop.getSize()*MUTATION_SIZE);
-      pop.select(SELECTION_SIZE);
+      Population pop;
 
-      }
+      if(conf.order_ordering_matrix) {
+
+        pop.generate(&matrix, INITIAL_POPULATION);
+
+        for(unsigned int i = 0; i < ITERATION_NUMBER; ++i)
+        {
       
-      matrix = *(pop.getFinalMatrix());
+        pop.mutate(pop.getSize()*MUTATION_SIZE);
+        pop.select(SELECTION_SIZE);
 
-      pop.displayFirstNorm();
+        }
+        
+        matrix = *(pop.getFinalMatrix());
+
+        pop.displayFirstNorm();
+      }
     }
-  }
 
-  file.close();
+    file.close();
+
+}
 
   /*// Build the order here. Let's do a sort in the meantime.
   std::sort( variables.begin(), variables.end()
