be6b61d28e5b2be070f213c940183a3d7c04f701
LucasAzais <lucasazais@gmail.com>
2014-05-31 17:54:39 +0200

    You can use the natural order without using a particular configuration file

--- src/mc/classic/make_order.cc
+++ src/mc/classic/make_order.cc
@@ -30,67 +30,64 @@ make_order(const conf::configuration& conf, statistics& stats, const pn::net& ne
   Matrix matrix = Matrix(net);
 
   std::ifstream file;
-  if(conf.matrix_conf_file_path){
 
-    file.open(*conf.matrix_conf_file_path);
+  if(conf.order_ordering_matrix) {
 
-    if(file.good())
-    {
+    if(conf.matrix_conf_file_path){
 
-      int buffer_size = 256;
-      char init_pop[buffer_size];
-      char it_num[buffer_size];
-      char mut_size[buffer_size];
-      char select_size[buffer_size];
+      file.open(*conf.matrix_conf_file_path);
 
-      file.getline(init_pop, buffer_size);
-      file.getline(it_num, buffer_size);
-      file.getline(mut_size, buffer_size);
-      file.getline(select_size, buffer_size);
+      if(file.good())
+      {
 
-      std::string init_pop_str (init_pop);
-      std::string it_num_str (it_num);
-      std::string mut_size_str (mut_size);
-      std::string select_size_str (select_size);
+        int buffer_size = 256;
+        char init_pop[buffer_size];
+        char it_num[buffer_size];
+        char mut_size[buffer_size];
+        char select_size[buffer_size];
 
-      init_pop_str.erase(0, 21);
-      it_num_str.erase(0, 19);
-      mut_size_str.erase(0, 16);
-      select_size_str.erase(0, 17);;
+        file.getline(init_pop, buffer_size);
+        file.getline(it_num, buffer_size);
+        file.getline(mut_size, buffer_size);
+        file.getline(select_size, buffer_size);
 
-      const unsigned int INITIAL_POPULATION = std::stoi( init_pop_str );
-      const unsigned int ITERATION_NUMBER = std::stoi( it_num_str );
-      const unsigned int MUTATION_SIZE = std::stoi( mut_size_str );
-      const unsigned int SELECTION_SIZE = std::stoi( select_size_str );
-    
-      Population pop;
+        std::string init_pop_str (init_pop);
+        std::string it_num_str (it_num);
+        std::string mut_size_str (mut_size);
+        std::string select_size_str (select_size);
 
-      if(conf.order_ordering_matrix) {
+        init_pop_str.erase(0, 21);
+        it_num_str.erase(0, 19);
+        mut_size_str.erase(0, 16);
+        select_size_str.erase(0, 17);;
 
-        pop.generate(&matrix, INITIAL_POPULATION);
-
-        for(unsigned int i = 0; i < ITERATION_NUMBER; ++i)
-        {
+        const unsigned int INITIAL_POPULATION = std::stoi( init_pop_str );
+        const unsigned int ITERATION_NUMBER = std::stoi( it_num_str );
+        const unsigned int MUTATION_SIZE = std::stoi( mut_size_str );
+        const unsigned int SELECTION_SIZE = std::stoi( select_size_str );
       
-        pop.mutate(pop.getSize()*MUTATION_SIZE);
-        pop.select(SELECTION_SIZE);
+        Population pop;
 
-        }
+          pop.generate(&matrix, INITIAL_POPULATION);
+
+          for(unsigned int i = 0; i < ITERATION_NUMBER; ++i)
+          {
         
-        matrix = *(pop.getFinalMatrix());
+          pop.mutate(pop.getSize()*MUTATION_SIZE);
+          pop.select(SELECTION_SIZE);
 
+          }
+          
+          matrix = *(pop.getFinalMatrix());
+
+        
       }
+
+      stats.matrix_order_duration = chrono::system_clock::now() - start;
+
+      file.close();
     }
-
-    stats.matrix_order_duration = chrono::system_clock::now() - start;
-
-    file.close();
-}
-
-  /*// Build the order here. Let's do a sort in the meantime.
-  std::sort( variables.begin(), variables.end()
-           , [](Variable* lhs, Variable* rhs)
-               {return lhs->position() < rhs->position();});*/
+  }
 
   // Return the order
   sdd::order_builder<sdd_conf> ob;
